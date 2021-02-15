ef0e95703c74d21b8a5d2417e7a4cd67bbfade20
alex_worm <batan_alex@icloud.com>
2021-02-15 21:52:09 +0300

    thats not alive

--- src/component/Board.js
+++ src/component/Board.js
@@ -1,5 +1,47 @@
 import React from "react";
 
+function Square(props) {
+  return (
+    <div
+      className="square"
+      onClick={props.onClick}
+      onContextMenu={props.onCMenu}
+    >
+      {props.value}
+    </div>
+  );
+}
+
+function Row(props) {
+  let row = [];
+
+  for (let j = 0; j < props.length; j++) {
+    row.concat([
+      <Square
+        value={this.props.squares[this.props.i * 10 + j]}
+        onClick={() => this.props.onClick(this.props.i * 10 + j)}
+        onCMenu={() => this.props.onCMenu(this.props.i * 10 + j)}
+      />,
+    ]);
+  }
+
+  return <div className="row">{row}</div>;
+}
+
 export default class Board extends React.Component {
-  state = {};
+  render() {
+    let rows = [];
+
+    // for (let i = 0; i < this.props.count; i++) {
+    //   rows.concat([
+    //     <Row
+    //       i={i}
+    //       onClick={() => this.props.onClick(i)}
+    //       onCMenu={() => this.props.onCMenu(i)}
+    //     />,
+    //   ]);
+    // }
+
+    return <div className="board">{rows}</div>;
+  }
 }

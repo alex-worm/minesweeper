22bc9bf5b239fc579debaab925cc729e2b7c5a6f
alex_worm <batan_alex@icloud.com>
2021-02-22 17:33:00 +0300

    edited default field size

--- src/component/Game.js
+++ src/component/Game.js
@@ -12,7 +12,7 @@ export default class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      length: 10,
+      length: 8,
       mines: 10,
       field: [],
       time: 0,
